a97058b406b88ca5599898958ed899aa752cc63b
IrsaKhan <irsakhan240@gmail.com>
2025-08-07 21:34:31 +0500

    Fix: merged conflict in Signature and Hero components

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // allowedDevOrigins: ['http://192.168.100.12:3000'], // or whatever your LAN IP + port is
+    allowedDevOrigins: ['http://192.168.100.12:3000'],
   },
 };
 
